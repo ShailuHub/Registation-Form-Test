f79430b02be9632e0e7fffb5c4fef6aaf2d6624b
Shailu <shailesh.respond@gmail.com>
2023-07-18 21:02:41 +0530

    test-3

--- index.js
+++ index.js
@@ -1,52 +1,6 @@
-const mouseover = document.getElementById("mouseover");
-const mouseout = document.getElementById("mouseout");
-const submit = document.getElementById("submit");
-
-mouseover.addEventListener("mouseover", function (event) {
-  event.preventDefault();
-
-  let name = document.getElementById("name").value;
-  let email = document.getElementById("email").value;
-  let phone = document.getElementById("phone").value;
-  let date = document.getElementById("date").value;
-  let time = document.getElementById("time").value;
-
-  console.log("Name: " + name);
-  console.log("Email: " + email);
-  ``;
-  console.log("Phone: " + phone);
-  console.log("Date: " + date);
-  console.log("Time: " + time);
-});
-
-mouseout.addEventListener("mouseout", function (event) {
-  event.preventDefault();
-  let name = document.getElementById("name").value;
-  let email = document.getElementById("email").value;
-  let phone = document.getElementById("phone").value;
-  let date = document.getElementById("date").value;
-  let time = document.getElementById("time").value;
-
-  console.log("Name: " + name);
-  console.log("Email: " + email);
-  console.log("Phone: " + phone);
-  console.log("Date: " + date);
-  console.log("Time: " + time);
-});
-
-submit.addEventListener("click", function (event) {
-  event.preventDefault();
-
-  let name = document.getElementById("name").value;
-  let email = document.getElementById("email").value;
-  let phone = document.getElementById("phone").value;
-  let date = document.getElementById("date").value;
-  let time = document.getElementById("time").value;
-
-  console.log("Name: " + name);
-  console.log("Email: " + email);
-  ``;
-  console.log("Phone: " + phone);
-  console.log("Date: " + date);
-  console.log("Time: " + time);
-});
+const headerTitle = document.getElementById("header-title");
+const mainHeader = document.getElementById("main-header");
+const title = document.getElementsByClassName("title");
+mainHeader.style.borderBottom = "solid 2px black";
+title[0].style.color = "green";
+title[0].style.fontWeight = 900;
