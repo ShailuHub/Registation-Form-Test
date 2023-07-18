0bb09a788e239a03cc2511bbddcb19a84243b088
Shailu <shailesh.respond@gmail.com>
2023-07-18 17:19:11 +0530

    Testing 2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mouseover = document.getElementById("mouseover");
 const mouseout = document.getElementById("mouseout");
+const submit = document.getElementById("submit");
 
 mouseover.addEventListener("mouseover", function (event) {
   event.preventDefault();
@@ -12,6 +13,7 @@ mouseover.addEventListener("mouseover", function (event) {
 
   console.log("Name: " + name);
   console.log("Email: " + email);
+  ``;
   console.log("Phone: " + phone);
   console.log("Date: " + date);
   console.log("Time: " + time);
@@ -27,6 +29,23 @@ mouseout.addEventListener("mouseout", function (event) {
 
   console.log("Name: " + name);
   console.log("Email: " + email);
+  console.log("Phone: " + phone);
+  console.log("Date: " + date);
+  console.log("Time: " + time);
+});
+
+submit.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let phone = document.getElementById("phone").value;
+  let date = document.getElementById("date").value;
+  let time = document.getElementById("time").value;
+
+  console.log("Name: " + name);
+  console.log("Email: " + email);
+  ``;
   console.log("Phone: " + phone);
   console.log("Date: " + date);
   console.log("Time: " + time);
