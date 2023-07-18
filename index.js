53fffc641b8de690a47737aa3a0d2aa8d021d10e
Shailu <shailesh.respond@gmail.com>
2023-07-18 17:01:27 +0530

    testing 1

---
+++ index.js
@@ -0,0 +1,33 @@
+const mouseover = document.getElementById("mouseover");
+const mouseout = document.getElementById("mouseout");
+
+mouseover.addEventListener("mouseover", function (event) {
+  event.preventDefault();
+
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let phone = document.getElementById("phone").value;
+  let date = document.getElementById("date").value;
+  let time = document.getElementById("time").value;
+
+  console.log("Name: " + name);
+  console.log("Email: " + email);
+  console.log("Phone: " + phone);
+  console.log("Date: " + date);
+  console.log("Time: " + time);
+});
+
+mouseout.addEventListener("mouseout", function (event) {
+  event.preventDefault();
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let phone = document.getElementById("phone").value;
+  let date = document.getElementById("date").value;
+  let time = document.getElementById("time").value;
+
+  console.log("Name: " + name);
+  console.log("Email: " + email);
+  console.log("Phone: " + phone);
+  console.log("Date: " + date);
+  console.log("Time: " + time);
+});
